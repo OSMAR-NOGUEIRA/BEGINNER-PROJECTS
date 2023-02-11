8c96e4beb5f769524ad3e40e6fc34f7ca3e5ff16
OSMAR-NOGUEIRA <112126780+OSMAR-NOGUEIRA@users.noreply.github.com>
2023-02-11 10:55:45 +0000

    Update JogoDaForca0010.py

--- JogoDaForca0010.py
+++ JogoDaForca0010.py
@@ -1,6 +1,3 @@
-import os
-
-
 palavrasecreta = input('digite a palavra secreta : ').upper()
 pontos = 6
 palavrasecretaemtracos = ['_'] * len(palavrasecreta)
@@ -8,30 +5,28 @@ letrasdigitadas = []
 
 print('\n' * 100)
 
-
-
 while True:
 
-    print(' '.join(palavrasecretaemtracos))   # join junta a lista e transforma em uma string
+    print(' '.join(palavrasecretaemtracos))  # join junta a lista e transforma em uma string
 
     print(f'\033[31m\nDigite 1 para sair\n\033[0;0m')
 
     letradigt = input('digite uma letra: ').upper()
 
-    if letradigt in letrasdigitadas:                             # se a letra ja ter sido digitada previamente
-        print(f'\033[31m'+f'{letradigt} JA FOI DIGITADA!'+'\033[0;0m')
+    if letradigt in letrasdigitadas:  # se a letra ja ter sido digitada previamente
+        print(f'\033[31m' + f'{letradigt} JA FOI DIGITADA!' + '\033[0;0m')
 
     if letradigt not in letrasdigitadas:
-        letrasdigitadas.append(letradigt)               # adiciona as letras digitadas nesta lista
+        letrasdigitadas.append(letradigt)  # adiciona as letras digitadas nesta lista
 
     print(f'LETRAS DIGITADAS : {letrasdigitadas},')
 
-    i = 0                         ##    para inicializar a contagem do indice da pset
-    if letradigt in palavrasecreta:      ##      verificando se a letra digitada esta na palavra secreta
-        for l in palavrasecreta:        ## percorrendo a palavrasecreta para achar o indice da letra
+    i = 0  ##    para inicializar a contagem do indice da pset
+    if letradigt in palavrasecreta:  ##      verificando se a letra digitada esta na palavra secreta
+        for l in palavrasecreta:  ## percorrendo a palavrasecreta para achar o indice da letra
             if letradigt == l:
-                palavrasecretaemtracos[i] = letradigt ## modificando a pset no indice correto
-            i += 1      ##continuando a contagem da posicao
+                palavrasecretaemtracos[i] = letradigt  ## modificando a pset no indice correto
+            i += 1  ##continuando a contagem da posicao
         print('Acertou!')
     else:
         pontos -= 1
@@ -44,10 +39,19 @@ while True:
         break
 
     if ''.join(palavrasecretaemtracos) == palavrasecreta:
-        print('\033[32m'+f'Voce ganhou! A palavra foi {palavrasecreta.upper()}'+'\033[0;0m')
+        print('\033[32m' + f'Voce ganhou! A palavra foi {palavrasecreta.upper()}' + '\033[0;0m')
+        print('Parabéns, você ganhou!')
+        print("       ___________      ")
+        print("      '._==_==_=_.'     ")
+        print("      .-\\:      /-.    ")
+        print("     | (|:.     |) |    ")
+        print("      '-|:.     |-'     ")
+        print("        \\::.    /      ")
+        print("         '::. .'        ")
+        print("           ) (          ")
+        print("         _.' '._        ")
+        print("        '-------'       ")  
         break
 
-
-    if letradigt == '1':   ## se o usuario quizer sair ele digita 1
+    if letradigt == '1':  ## se o usuario quizer sair ele digita 1
         break
-
